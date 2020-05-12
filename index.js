096380ebd925d172dcec7cc0c76fe0e20d5cd1b8
domiebett <dbett49@gmail.com>
2020-05-12 11:50:44 +0300

    move navigation to index.html

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-const nav = `
-<section class="left">
-Dominic Bett
-</section>
-<section class="right">
-<ul>
-    <li class="appLink" data-url="myProfile">My Profile</li>
-    <li class="appLink" data-url="myWork">My Work</li>
-    <li class="appLink" data-url="funAndGames">Fun and Games</li>
-</ul>
-</section>`;
-
 let currentUrl;
 
 document.addEventListener('DOMContentLoaded', async () => {
-    const navElement = await document.getElementById('navigation');
-    navElement.innerHTML = nav;
-
     let homePage = 'myProfile';
     loadContent(homePage);
 
@@ -69,39 +54,4 @@ async function loadJsInIframe(url) {
         await script.setAttribute('type', 'text/javascript');
         iframe.find('body').append(script);
     });
-}
-
-async function loadJsFile(fileName) {
-    const className = 'addedJs';
-    const body = document.getElementsByTagName('body')[0];
-
-    const scripts = document.querySelectorAll(`.${className}`);
-    scripts.forEach((script) => {
-        body.removeChild(script);
-    });
-
-    let script = await document.createElement('script');
-    script.className = className;
-    await script.setAttribute('type', 'text/javascript');
-    await script.setAttribute('src', `./static/js/${fileName}.js`);
-
-    return body.appendChild(script);
-}
-
-async function loadCssFile(fileName) {
-    const className = 'addedCss';
-    const head = document.getElementsByTagName('head')[0];
-
-    const styles = document.querySelectorAll(`.${className}`);
-    styles.forEach((style) => {
-        head.removeChild(style);
-    });
-
-    let link = await document.createElement('link');
-    link.className = className;
-    await link.setAttribute('type', 'text/css');
-    await link.setAttribute('href', `./static/css/${fileName}.css`);
-    await link.setAttribute('rel', 'stylesheet');
-
-    return head.appendChild(link);
 }
