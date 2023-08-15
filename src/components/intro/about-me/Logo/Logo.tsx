873b7def83095fd1007261b3233a9c3959452e0d
Dominic Bett <dbett49@gmail.com>
2023-08-15 20:12:14 +0300

    responsiveness fix

--- src/components/intro/about-me/Logo/Logo.tsx
+++ src/components/intro/about-me/Logo/Logo.tsx
@@ -1,17 +1,22 @@
-import { nameGif } from 'assets/gifs';
-import './Logo.scss';
+import { nameGif } from "assets/gifs";
+import "./Logo.scss";
 
 import { DLogo } from "assets/icons";
+import { ScreenSizeContext } from "contexts/Contexts";
 
 const Logo = () => {
   return (
-    <div className="Logo">
-      <img src={nameGif} alt="d-logo" />
-      <div className="name">
-        <h2>Dominic</h2>
-        <h2>Bett</h2>
-      </div>
-    </div>
+    <ScreenSizeContext.Consumer>
+      {(screenSize) => (
+        <div className={`Logo ${screenSize}`}>
+          <img src={nameGif} alt="d-logo" />
+          <div className="name">
+            <h2>Dominic</h2>
+            <h2>Bett</h2>
+          </div>
+        </div>
+      )}
+    </ScreenSizeContext.Consumer>
   );
 };
 
