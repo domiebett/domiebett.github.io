eafcfebe5d70d265fa4c9b11a1f0557b8f3ea6c8
Dominic Bett <dbett49@gmail.com>
2024-12-18 18:33:50 +0300

    Sizing and contrast

--- src/components/darkdev/PersonalInfo/Links/Links.tsx
+++ src/components/darkdev/PersonalInfo/Links/Links.tsx
@@ -6,8 +6,8 @@ import styles from "./Links.module.scss";
 const Links = () => {
   return (
     <div className={styles.container}>
-      <Link to={Resume} target="_blank">View Resume</Link>
-      <Link to={'/portfolio'}>View Portfolio</Link>
+      <Link to={Resume} target="_blank" className={styles.resumeLink}>Open Resume</Link>
+      <Link to={'/portfolio'} className={styles.portfolioLink}>View Projects</Link>
     </div>
   );
 };
--- src_old/components/my-info/IView.ts
+++
@@ -1,3 +0,0 @@
-export interface ViewProps {
-  styles: Record<string, string | number>;
-}
